e992b591d5e3f51a5d4927297c080638bbefb2c5
UltraFizzyMel <2344445@students.wits.ac.za>
2024-05-19 23:11:58 +0200

    Grid and Movement
    Basic grid system with different tile prefabs (with correct naming from A1-G7). Draggable pieces (cannot drag onto a tile with another piece but can drag onto any tile at this point)

---
+++ Assets/SCripts/TileNew.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TileNew : MonoBehaviour, IDropHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 1)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+            draggableItem.parentAfterDrag = transform;
+        }
+    }
+}
